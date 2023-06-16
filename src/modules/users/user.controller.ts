76e906b04031e8e818af15d126c56946253c4e36
abdulwahabFolio3 <abdulwahab@folio3.com>
2023-06-16 12:58:57 +0500

    UserController error handling

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,35 +1,99 @@
-import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
-import { User } from './user.schema';
+import { Controller, Post, Body, Get, Param, Put, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
-import { HashMiddleware } from 'src/middlewares/hash.middleware';
+import { User } from './user.schema';
+import { ApiResponse } from './../../core/api/api.interface';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  async createUser(@Body() user: User): Promise<User> {
-    return this.userService.create(user);
+  async createUser(@Body() user: User): Promise<ApiResponse<User | null>> {
+    try {
+      await this.userService.create(user);
+      return {
+        status: 'success',
+        data: user,
+        message: 'User created successfully',
+      };
+    } catch (error) {
+      return {
+        status: 'error',
+        data: null,
+        message: 'Error creating user',
+      };
+    }
   }
 
   @Get()
-  async getAllUsers(): Promise<User[]> {
-    return this.userService.findAll();
+  async findAllUsers(): Promise<ApiResponse<User[] | null>> {
+    try {
+      const users = await this.userService.findAll();
+      return {
+        status: 'success',
+        data: users,
+        message: 'Users fetched successfully',
+      };
+    } catch (error) {
+      return {
+        status: 'error',
+        data: null,
+        message: 'Error fetching users',
+      };
+    }
   }
 
   @Get(':id')
-  async getUserById(@Param('id') id: string): Promise<User> {
-    return this.userService.findById(id);
+  async findUserById(@Param('id') id: string): Promise<ApiResponse<User | null>> {
+    try {
+      const user = await this.userService.findById(id);
+      return {
+        status: 'success',
+        data: user,
+        message: 'User fetched successfully',
+      };
+    } catch (error) {
+      return {
+        status: 'error',
+        data: null,
+        message: 'Error fetching user',
+      };
+    }
   }
 
   @Put(':id')
-  async updateUser(@Param('id') id: string, @Body() user: User): Promise<User> {
-    return this.userService.update(id, user);
+  async updateUser(@Param('id') id: string, @Body() user: User): Promise<ApiResponse<User | null>> {
+    try {
+      const updatedUser = await this.userService.update(id, user);
+      return {
+        status: 'success',
+        data: updatedUser,
+        message: 'User updated successfully',
+      };
+    } catch (error) {
+      return {
+        status: 'error',
+        data: null,
+        message: 'Error updating user',
+      };
+    }
   }
 
   @Delete(':id')
-  async deleteUser(@Param('id') id: string): Promise<User> {
-    return this.userService.delete(id);
+  async deleteUser(@Param('id') id: string): Promise<ApiResponse<User | null>> {
+    try {
+      const deletedUser = await this.userService.delete(id);
+      return {
+        status: 'success',
+        data: deletedUser,
+        message: 'User deleted successfully',
+      };
+    } catch (error) {
+      return {
+        status: 'error',
+        data: null,
+        message: 'Error deleting user',
+      };
+    }
   }
 }
-
