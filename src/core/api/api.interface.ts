00039147b03b15f541097dd6f63768ce015d4595
abdulwahabFolio3 <abdulwahab@folio3.com>
2023-06-22 22:37:48 +0500

    Response

--- src/core/api/api.interface.ts
+++ src/core/api/api.interface.ts
@@ -3,3 +3,11 @@ export interface ApiResponse<T> {
     data: T;
     message: string;
 }
+
+export function Response<T>(status: 'success' | 'error', data: T, message: string): ApiResponse<T> {
+    return {
+      status,
+      data,
+      message,
+    };
+  }
