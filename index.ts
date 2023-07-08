5191992d29e6487b24963ea2804c44a4b6056c24
abdulwahabFolio3 <abdulwahab@folio3.com>
2023-07-08 14:03:12 +0500

    Firebase deploy to cloud funtions setup

--- index.ts
+++ index.ts
@@ -3,14 +3,17 @@ import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
 import * as functions from 'firebase-functions';
 import { AppModule } from './src/app.module';
+
 const expressServer = express();
+
 const createFunction = async (expressInstance): Promise<void> => {
   const app = await NestFactory.create(
     AppModule,
     new ExpressAdapter(expressInstance),
   );
-await app.init();
+  await app.init();
 };
+
 export const api = functions.https.onRequest(async (request, response) => {
   await createFunction(expressServer);
   expressServer(request, response);
