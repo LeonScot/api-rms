df5dbaa6a1bfc18964f485b804deb0d6208c0513
abdulwahabFolio3 <abdulwahab@folio3.com>
2023-07-15 15:42:46 +0500

    createHasFile

--- src/modules/subscription/subscription.service.ts
+++ src/modules/subscription/subscription.service.ts
@@ -4,13 +4,27 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Subscription } from './subscription.schema';
 import { CrudService } from 'src/core/api/crud.service';
+import { from, lastValueFrom, of, pipe, switchMap } from 'rxjs';
+import { AttachmentService } from '../attachment/attachment.service';
 
 @Injectable()
 export class SubscriptionService extends CrudService<Subscription> {
 
     public query: object;
 
-    constructor(@InjectModel(Subscription.name) private readonly subscriptionModel: Model<Subscription>) {
+    constructor(@InjectModel(Subscription.name) private readonly subscriptionModel: Model<Subscription>, private attachmentService: AttachmentService) {
         super(subscriptionModel);
+    }
+
+    public createHasFile(subscription: Subscription) {
+        const subscription$ = from(this.create(subscription)).pipe(
+            switchMap(async res => {
+                const attachment = await this.attachmentService.findByUrl(res.image);
+                attachment.isLinked = true;
+                this.attachmentService.update(attachment._id, attachment);
+                return res;
+            })
+        );
+        return lastValueFrom(subscription$);
     }
 }
