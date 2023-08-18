2d3159034a5d686be539c48dbc0927251c821e51
abdulwahabFolio3 <abdulwahab@folio3.com>
2023-08-18 16:39:32 +0500

    added new methods, direct query will be used for all

--- src/core/api/crud.service.ts
+++ src/core/api/crud.service.ts
@@ -56,6 +56,43 @@ export abstract class CrudService<T> {
         return { data, totalCount };
     }
 
+
+    /** Suitable when use in a loop */
+    async findAllDirectQuery(queryDirect: object | null = null, page: IPagination | null = null, sort: ISort | null = null ): Promise<{ data: T[]; totalCount: number }> {
+        const query = this.model.find(queryDirect);
+        
+        // Exclude documents
+        // if (this.excludedQuery) {
+        //     for (const key in this.excludedQuery) {
+        //         if (Object.prototype.hasOwnProperty.call(this.excludedQuery, key)) {
+        //             const element = this.excludedQuery[key];
+        //             query.where(key).nin(element);
+        //         }
+        //     }
+        // }
+        
+        // Apply sorting if sort options are provided
+        if (sort) {
+          const { field, order } = sort;
+          const sortOrder = order === 'desc' ? -1 : 1;
+          query.sort({ [field]: sortOrder });
+        }
+        
+        // Apply pagination if page options are provided
+        if (page && page.pageNumber && page.limit && page.pageNumber > 0 && page.limit > 0) {
+          const skip = (page.pageNumber - 1) * page.limit;
+          query.skip(skip).limit(page.limit);
+        }
+        
+        // Execute the query and populate reference objects
+        const data = await query.populate(this.refObjectNames).exec();
+        
+        // Count the total number of documents
+        const totalCount = await this.model.countDocuments(queryDirect).exec();
+        
+        return { data, totalCount };
+    }
+
     async count(): Promise<number> {
         return this.model.countDocuments(this.query).exec();
     }
@@ -64,12 +101,28 @@ export abstract class CrudService<T> {
         return this.model.findById(id).populate(this.refObjectNames).exec();
     }
 
-    async findOneByQuery(query: object = {}) {
-        return this.model.findOne(query).populate(this.refObjectNames).exec();
+    async findOneByQuery(query: object = {}, sort: ISort | null = null) {
+
+        const queryExe = this.model.findOne(query);
+        if (sort !== null) {
+            const { field, order } = sort;
+            const sortOrder = order === 'desc' ? -1 : 1;
+            queryExe.sort({ [field]: sortOrder });
+        }
+        return queryExe.populate(this.refObjectNames).exec();
     }
 
     async update(id: string, rec: T): Promise<T> {
         return this.model.findByIdAndUpdate(id, rec, { new: true }).exec();
+    }
+
+    async updateManyByQuery(query: object, update: object): Promise<number> {
+        // Update multiple documents that match the query with the provided update
+        const updateExe = { $set: update };
+        const updateResult = await this.model.updateMany(query, updateExe).exec();
+    
+        // Return the number of documents updated
+        return updateResult.modifiedCount;
     }
 
     async delete(id: string): Promise<T> {
